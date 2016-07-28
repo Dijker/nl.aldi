bea9d6b4562518d5bd8be626254bf7f4d4d15027
baskiers <kiers.bas@gmail.com>
2016-07-28 18:08:10 +0200

    fixed breaking changes in 0.9

--- drivers/quigg/driver.js
+++ drivers/quigg/driver.js
@@ -21,19 +21,29 @@ var tempdata = {
 				var debouncer = new Debouncer(1000);
 
 				var Signal = Homey.wireless('433').Signal;
-				signal = new Signal({   
-					sof: [], //Start of frame
-				   	eof: [2970], //End of frame
-					words: [
-						[460, 1050],	// 0
-						[960, 560]	// 1
-					],
-					interval: 7060, //Time between repititions
-					repetitions: 20,
-					sensitivity: 0.7, 
-					minimalLength: 24,
-	   				maximalLength: 24
-				});	
+				signal = new Signal('quigg');
+
+				signal.numberToBitArray = function(number, bit_count) {
+					var result = [];
+					for (var i = 0; i < bit_count; i++)
+						result[i] = (number >> i) & 1;
+					return result;
+				};
+
+				signal.bitArrayToNumber = function(bits) {
+					return parseInt(bits.join(""),2);
+				};
+
+				signal.bitStringToBitArray = function(str) {
+					var result = [];
+					for (var i = 0; i < str.length; i++)
+						result.push(str.charAt(i) == '1' ? 1 : 0);
+					return result;
+				};
+
+				signal.bitArrayToString = function(bits) {
+					return bits.join("");
+				};
 
 				signal.register(function( err, success ){
 				    if(err != null){
